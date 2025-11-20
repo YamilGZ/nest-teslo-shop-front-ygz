6ec5bc3546ce09ccb902174b460655078a1fd701
Yamil Zaracho <yamil.zaracho@grupoepem.com.py>
2025-11-20 14:59:42 -0300

    Pruebas de Websocket

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,37 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>Websocket - Cliente</h1>
+    <h2>Websocket - Cliente</h2>
 
-    <span>offline</span>
+    <input id="jwt-token" placeholder="Json Web Token">
+    <button id="btn-connect">Connect</button>
+
+    <br>
+    <br>
+    <span id="server-status">offline</span>
+
+    <ul id="clients-ul"> 
+      <li>Client 1</li>
+    </ul>
+
+    <form id="message-form"> 
+      <input type="text" id="message-input" placeholder="Message">
+    </form>
+
+    <h3>Messages</h3>
+    <ul id="messages-ul"></ul>
+
   </div>
 `
 
-connectToServer();
+//connectToServer();
+
+const btnConnect = document.querySelector<HTMLButtonElement>('#btn-connect');
+const jwtToken = document.querySelector<HTMLInputElement>('#jwt-token');
+
+btnConnect!.addEventListener('click', () => {
+
+  if (jwtToken!.value.trim().length <= 0) return alert('JWT Token is required');
+
+  connectToServer(jwtToken!.value.trim());
+});
